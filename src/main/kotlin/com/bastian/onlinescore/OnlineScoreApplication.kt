57a79a96709b2216d3b199f43c7d4f5a9253a826
Bastian Somon <bastian.somon@gmail.com>
2022-11-28 16:35:39 +0100

    Init commit

---
+++ src/main/kotlin/com/bastian/onlinescore/OnlineScoreApplication.kt
@@ -0,0 +1,11 @@
+package com.bastian.onlinescore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineScoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineScoreApplication>(*args)
+}
