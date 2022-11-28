c6216bf4b3e8415ee1cc6f4b990b156957e8b221
Bastian Somon <bastian.somon@gmail.com>
2022-11-28 17:00:12 +0100

    Maven

---
+++ src/main/java/com/bastiansmn/scoreserver/ScoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.bastiansmn.scoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastiansmn/scoreserver/ScoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastiansmn.scoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
